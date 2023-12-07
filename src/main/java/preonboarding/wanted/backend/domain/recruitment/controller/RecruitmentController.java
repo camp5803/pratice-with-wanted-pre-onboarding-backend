4bb3e730398e16bd7a3a3e43db9cabe40a4628b3
Jiyong Jung <hickerd@sch.ac.kr>
2023-12-08 04:41:38 +0900

    [Feat] 컨트롤러 <-> 서비스 간 Dto를 통하여 통신하도록 변경, 검색 기능을 위한 메소드 추가

--- src/main/java/preonboarding/wanted/backend/domain/recruitment/controller/RecruitmentController.java
+++ src/main/java/preonboarding/wanted/backend/domain/recruitment/controller/RecruitmentController.java
@@ -2,66 +2,88 @@ package preonboarding.wanted.backend.domain.recruitment.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import preonboarding.wanted.backend.domain.recruitment.dto.RecruitmentDto;
 import preonboarding.wanted.backend.domain.recruitment.model.Recruitment;
 import preonboarding.wanted.backend.domain.recruitment.service.RecruitmentService;
+import preonboarding.wanted.backend.domain.user.model.EnterpriseUser;
+import preonboarding.wanted.backend.domain.user.service.EnterpriseUserService;
+
+import java.util.List;
 
 @Controller
+@Transactional
 public class RecruitmentController {
 
     private final RecruitmentService recruitmentService;
+    private final EnterpriseUserService enterpriseUserService;
 
     @Autowired
-    public RecruitmentController(RecruitmentService recruitmentService) {
+    public RecruitmentController(RecruitmentService recruitmentService, EnterpriseUserService enterpriseUserService) {
         this.recruitmentService = recruitmentService;
+        this.enterpriseUserService = enterpriseUserService;
     }
 
     @GetMapping("/recruitment")
     @ResponseBody
-    public Iterable<Recruitment> getRecruitments() {
+    public List<RecruitmentDto> getRecruitments() {
         return recruitmentService.getRecruitments();
     }
 
     @GetMapping("/recruitment/{id}")
     @ResponseBody
-    public Recruitment getRecruitment(@PathVariable Long id) {
+    public RecruitmentDto getRecruitment(@PathVariable Long id) {
         return recruitmentService.getRecruitment(id);
+    }
+
+    @GetMapping("/recruitment/search")
+    @ResponseBody
+    public List<Recruitment> searchRecruitment(@RequestParam("query") String query) {
+        return recruitmentService.searchRecruitment(query);
     }
 
     @PostMapping("/recruitment")
     @ResponseBody
-    public Recruitment createRecruitment(
+    @Transactional
+    public RecruitmentDto createRecruitment(
+            @RequestParam("enterpriseUserId") Long enterpriseUserId,
             @RequestParam("position") String position,
             @RequestParam("guarantee") Long guarantee,
             @RequestParam("content") String content,
             @RequestParam("techStack") String techStack
     ) {
-        Recruitment recruitment = Recruitment.builder()
+        EnterpriseUser enterpriseUser = enterpriseUserService.findEnterpriseUserById(enterpriseUserId).orElseThrow(
+                () -> new IllegalArgumentException("기업 정보 없음")
+        );
+
+        RecruitmentDto recruitmentDto = RecruitmentDto.builder()
                 .position(position)
                 .guarantee(guarantee)
                 .content(content)
                 .techStack(techStack)
+                .enterpriseUser(enterpriseUser)
                 .build();
 
-        return recruitmentService.createRecruitment(recruitment);
+        return recruitmentService.createRecruitment(recruitmentDto);
     }
 
     @PatchMapping("/recruitment/{id}")
     @ResponseBody
-    public Recruitment updateRecruitment(
+    public RecruitmentDto updateRecruitment(
             @PathVariable Long id,
             @RequestParam("position") String position,
             @RequestParam("guarantee") Long guarantee,
             @RequestParam("content") String content,
             @RequestParam("techStack") String techStack
     ) {
-        Recruitment originalRecruitment = recruitmentService.getRecruitment(id);
-        Recruitment modifiedRecruitment = Recruitment.builder()
+        RecruitmentDto originalRecruitment = recruitmentService.getRecruitment(id);
+        RecruitmentDto modifiedRecruitment = RecruitmentDto.builder()
                 .id(id)
-                .position(position != null ? position : originalRecruitment.getPosition())
-                .guarantee(guarantee != null ? guarantee : originalRecruitment.getGuarantee())
-                .content(content != null ? content : originalRecruitment.getContent())
-                .techStack(techStack != null ? techStack : originalRecruitment.getTechStack())
+                .position(position != null ? position : originalRecruitment.position())
+                .guarantee(guarantee != null ? guarantee : originalRecruitment.guarantee())
+                .content(content != null ? content : originalRecruitment.content())
+                .techStack(techStack != null ? techStack : originalRecruitment.techStack())
                 .build();
 
         return recruitmentService.updateRecruitment(modifiedRecruitment);
--- src/main/java/preonboarding/wanted/backend/domain/recruitment/service/RecruitmentService.java
+++ src/main/java/preonboarding/wanted/backend/domain/recruitment/service/RecruitmentService.java
@@ -3,39 +3,77 @@ package preonboarding.wanted.backend.domain.recruitment.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import preonboarding.wanted.backend.domain.recruitment.dto.RecruitmentDto;
 import preonboarding.wanted.backend.domain.recruitment.model.Recruitment;
 import preonboarding.wanted.backend.domain.recruitment.repository.RecruitmentRepository;
+import preonboarding.wanted.backend.domain.user.model.EnterpriseUser;
+import preonboarding.wanted.backend.domain.user.repository.EnterpriseUserRepository;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
 public class RecruitmentService {
 
+    private final EnterpriseUserRepository enterpriseUserRepository;
     private final RecruitmentRepository recruitmentRepository;
 
     @Autowired
-    public RecruitmentService(RecruitmentRepository recruitmentRepository) {
+    public RecruitmentService(EnterpriseUserRepository enterpriseUserRepository, RecruitmentRepository recruitmentRepository) {
+        this.enterpriseUserRepository = enterpriseUserRepository;
         this.recruitmentRepository = recruitmentRepository;
     }
 
-    public Recruitment createRecruitment(Recruitment recruitment) {
-        return recruitmentRepository.save(recruitment);
+    public RecruitmentDto createRecruitment(RecruitmentDto recruitmentDto) {
+        return RecruitmentDto.from(recruitmentRepository.save(recruitmentDto.toEntity()));
     }
 
-    public Recruitment updateRecruitment(Recruitment recruitment) {
-        return recruitmentRepository.save(recruitment);
+    public RecruitmentDto updateRecruitment(RecruitmentDto recruitmentDto) {
+        return RecruitmentDto.from(recruitmentRepository.save(recruitmentDto.toEntity()));
     }
 
-    public Recruitment getRecruitment(Long id) {
-        return recruitmentRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 공고가 없습니다."));
+    public RecruitmentDto getRecruitment(Long id) {
+        return RecruitmentDto.from(
+                recruitmentRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 공고가 없습니다."))
+        );
     }
 
-    public List<Recruitment> getRecruitments() {
-        return recruitmentRepository.findAll();
+    public List<RecruitmentDto> getRecruitments() {
+        return convertToDto(recruitmentRepository.findAll());
     }
 
     public void deleteRecruitment(Long id) {
         recruitmentRepository.deleteById(id);
+    }
+
+    public List<Recruitment> searchRecruitment(String query) {
+        Optional<EnterpriseUser> enterpriseInformation = enterpriseUserRepository.findByNameContaining(query);
+        List<Recruitment> byName = enterpriseInformation
+                .map(recruitmentRepository::findByEnterpriseUser)
+                .orElseGet(Collections::emptyList);
+        List<Recruitment> byPosition = recruitmentRepository.findByPositionContaining(query);
+        List<Recruitment> byTechStack = recruitmentRepository.findByTechStackContaining(query);
+        List<Recruitment> byContent = recruitmentRepository.findByContentContaining(query);
+
+        // List들을 통합하여 아래에서 중복 제거
+        List<List<Recruitment>> results = Arrays.asList(byName, byPosition, byTechStack, byContent);
+
+        return mergeAndDistinctLists(results);
+    }
+
+    private <T> List<T> mergeAndDistinctLists(List<List<T>> lists) {
+        return lists.stream()
+                .flatMap(List::stream)
+                .distinct()
+                .toList();
+    }
+
+    private List<RecruitmentDto> convertToDto(List<Recruitment> recruitments) {
+        return recruitments.stream()
+                .map(RecruitmentDto::from)
+                .toList();
     }
 }
